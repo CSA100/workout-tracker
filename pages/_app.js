d2e5aab9660296457df180fed20674f60241c4ed
CSA100 <chaanan100@gmail.com>
2021-03-29 12:32:28 +0800

    saved changes

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,20 @@ const theme = extendTheme({
     heading: "Archivo",
     body: "Archivo",
   },
+  styles: {
+    global: (props) => ({
+      a: {
+        color: props.colorMode === "dark" ? "teal.300" : "teal.500",
+      },
+    }),
+    body: {
+      userSelect: "none",
+    },
+  },
 });
+
+// help me make a global style that prevents the user from highliting text. So basically a prop that is passed to
+// the component that will prevent highlight on most web browsers. thanks
 
 function MyApp({ Component, pageProps }) {
   return (
